bb12904f0d18564a61e740b3d7053e763f16cc9d
ji-hyon <gyg03134@gmail.com>
2023-07-18 22:35:25 +0900

    Feat: 네비게이션 바 마이페이지 추가

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,8 +12,11 @@ function NavBar() {
             <img src={process.env.PUBLIC_URL + "/assets/logo.png"} alt="" />
           </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="practice">AI 언어재활</Nav.Link>
-            <Nav.Link href="consulting">치료 상담</Nav.Link>
+            <Nav.Link href="practice">언어재활</Nav.Link>
+            <Nav.Link href="consulting">치료상담</Nav.Link>
+          </Nav>
+          <Nav className='ms-auto'>
+            <Nav.Link href="profile">프로필</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
