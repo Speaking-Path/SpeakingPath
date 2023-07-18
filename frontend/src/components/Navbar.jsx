b2f6e39fc7bd64c6ed4039b6229f332e3df1d338
Yeoni <siunaus224@gmail.com>
2023-07-19 01:03:44 +0900

    Fix: 네비게이션바, 회원가입 네비게이션바 이동 방법 수정, 스타일 오류 수정,회원가입 컴포넌트 수정

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
+// 네비게이션 바
+
 import './Navbar.module.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
@@ -12,13 +15,26 @@ function NavBar() {
             <img src={process.env.PUBLIC_URL + "/assets/logo.png"} alt="" />
           </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="practice">AI 언어재활</Nav.Link>
-            <Nav.Link href="consulting">치료 상담</Nav.Link>
+            <NavLink
+              style={({ isActive }) => { return { fontWeight: isActive ? "bold" : "", } }}
+              to="practice">
+              AI 언어재활</NavLink>
+            <NavLink
+              style={({ isActive }) => { return { fontWeight: isActive ? "bold" : "", } }}
+              to="consulting">
+              치료 상담</NavLink>
+          </Nav>
+          <Nav className="me-auto">
+            <NavLink
+              style={({ isActive }) => { return { fontWeight: isActive ? "bold" : "", } }}
+              to="account/signup">
+              회원가입</NavLink>
           </Nav>
         </Container>
       </Navbar>
     </>
   );
 }
+
 
 export default NavBar;
