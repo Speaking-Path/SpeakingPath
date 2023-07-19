d41f5fdfc5337e058c95acf8b99eb932f0bfa284
박은규 <parkeg1223@gmail.com>
2023-07-19 10:04:07 +0900

    Fix: 프: 프로젝트 세팅

--- BE/spkpath/resources/sample_data.sql
+++
@@ -1,4 +0,0 @@
-use spkpath;
-
-insert into user_tb(`user_id`, `user_pwd`, `user_name`, `user_email`, `user_info`, `user_age`, `user_sex`, `user_grade`)
-values('ssafy', 'ssafy', '김싸피', 'ssafy@ssafy.com', '안녕하세요', 20, 'M', '일반회원');
