e5e5a2f17359814733c7687db52ecb59000eb2da
박은규 <parkeg1223@gmail.com>
2023-07-19 10:07:30 +0900

    Docs: sql schema 추가

---
+++ BE/spkpath/resources/sample_data.sql
@@ -0,0 +1,4 @@
+use spkpath;
+
+insert into user_tb(`user_id`, `user_pwd`, `user_name`, `user_email`, `user_info`, `user_age`, `user_sex`, `user_grade`)
+values('ssafy', 'ssafy', '김싸피', 'ssafy@ssafy.com', '안녕하세요', 20, 'M', '일반회원');
