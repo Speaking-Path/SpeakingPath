0de9d38403effaeb5a42476a7631b35857cff127
서지현 <gyg03134@naver.com>
2023-07-20 19:43:21 +0900

    Feat: 프로필페이지 화면 미리보기 기능 구현

--- frontend/src/components/profile/Preview.jsx
+++ frontend/src/components/profile/Preview.jsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import styles from './Preview.module.css';
+import MyCamera from './MyCamera'; 
 
 const Preview = ({ isOpen, onClose, children, size }) => {
   if (!isOpen) return null;
@@ -7,7 +9,10 @@ const Preview = ({ isOpen, onClose, children, size }) => {
     <div className={styles.preview}>
       <div className={`${styles.previewContent} ${size === 'large' ? styles.large : ''}`}>
         {children}
-        <button onClick={onClose}>닫기</button>
+        <MyCamera />
+        <div>
+          <button onClick={onClose}>닫기</button>
+        </div>
       </div>
     </div>
   );
@@ -15,19 +20,3 @@ const Preview = ({ isOpen, onClose, children, size }) => {
 
 export default Preview;
 
-// import styles from './Preview.module.css';
-
-// const Preview = ({ isOpen, onClose, children, size }) => {
-//   if (!isOpen) return null;
-
-//   return (
-//     <div className={styles.Preview}>
-//       <div className={`${styles.PreviewContent} ${size === 'large' ? styles.large : ''}`}>
-//         {children}
-//         <button onClick={onClose}>닫기</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Preview;
