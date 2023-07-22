cad2836d3f6df5ff3c86cc67174d5fbadb0394a2
오연주 <siunaus224@gmail.com>
2023-07-22 16:39:06 +0900

    Feat: 로그인, 회원가입 기능 추가 상세설정 추가, 입력별 알림 추가

--- frontend/src/components/Signup/AuthAPI.js
+++ frontend/src/components/Signup/AuthAPI.js
@@ -16,34 +16,50 @@ export const AuthApi = axios.create({
 })
 
 
-export const signUp = async ({userName, email, phoneNumber, id, password}) => {
-  const data = {
-    "userName" : userName,
-    "userEmail" : email,
-    "userPhone" : phoneNumber,
-    "userId" : id,
-    "userPwd" : password,
+
+export const clickSignup = async function (e, data) {
+  e.preventDefault()
+  const userInfo = {
+    "userName" : data.userName,
+    "userEmail" : data.email,
+    "userPhone" : data.phoneNumber,
+    "userId" : data.id,
+    "userPwd" : data.password,
   }
-  const response = await AuthApi.post(`account/signup`, data)
-  return response.data
+  try {
+    const response = await AuthApi.post(`account/signup`, userInfo)
+    return response.data
+  } catch(err) {
+    alert('정보를 다시 한번 확인해주세요')
+    console.log(err)
+    return err.message
+  }
 }
 
-export const consultSignUp = async ({
-  userName, email, id, phoneNumber, password, team, exp, tag, boundry }) => {
-  const data = {
-    "userName" : userName,
-    "userEmail" : email,
-    "userPhone" : phoneNumber,
-    "userId" : id,
-    "userPwd" : password,
-    "cnslrTeam" : team,
-    "cnslrExp" : exp, 
-    "cnslrTag" : tag,
-    "cnslrBoundry" : boundry,
+
+
+export const clickConsultantSignup = async function (e, data) {
+  console.log(data)
+  const userInfo = {
+    "userName" : data.userName,
+    "userEmail" : data.email,
+    "userPhone" : data.phoneNumber,
+    "userId" : data.id,
+    "userPwd" : data.password,
+    "cnslrTeam" : data.team,
+    "cnslrExp" : data.exp, 
+    "cnslrTag" : data.tag,
+    "cnslrBoundry" : data.boundry,
     "userGrade" : "상담사",
   }
-  const response = await AuthApi.post(`account/signup`, data)
-  return response.data
+  try {
+    const response = await AuthApi.post(`account/signup`, userInfo)
+    return response.data
+  } catch(err) {
+    alert('정보를 다시 한번 확인해주세요')
+    console.log(err)
+    return err.message
+  }
 }
 
 
@@ -56,21 +72,41 @@ export const login = async ({ id, password }) => {
   return response.data
 }
 
-export const checkEmailApi = function(email) {
+
+
+export const checkEmailApi = function (e, email, setEmailMessage, setIsEmail) {
+  e.preventDefault()
   axios
   .get("http://localhost:8080/account/checkemail",
   {params : { "userEmail" : email}})
   .then((res)=>{
-    alert(res.data)
+    if (res.data === "success") {
+      setEmailMessage("사용 가능한 이메일입니다.")
+      setIsEmail(true)
+    } else {
+      setEmailMessage("이미 존재하는 이메일입니다.")
+      setIsEmail(false)
+    }
   })
   .catch((err)=>{
+    console.log(err)
   })
 }
 
-export const checkIdApi = function(id) {
+export const checkIdApi = function (e, id, setIdMessage, setIsId) {
+  e.preventDefault()
   axios.get("http://localhost:8080/account/checkid",
   {params : {"userId" : id}})
   .then((res)=>{
-    console.log(res)
+    if (res.data === "success") {
+      setIdMessage("사용 가능한 아이디입니다.")
+      setIsId(true)
+    } else {
+      setIdMessage("이미 존재하는 아이디입니다.")
+      setIsId(false)
+    }
+  })
+  .catch((err)=>{
+    console.log(err)
   })
 }
--- frontend/src/components/Signup/SignupFunc.jsx
+++ frontend/src/components/Signup/SignupFunc.jsx
@@ -1,4 +1,4 @@
-import { signUp, consultSignUp, login, checkEmailApi, checkIdApi } from "./AuthAPI"
+import { login } from "./AuthAPI"
 
 export const onChangeEmail = function(e, setEmail, setEmailMessage) {
   const currentEmail = e.target.value
@@ -7,25 +7,31 @@ export const onChangeEmail = function(e, setEmail, setEmailMessage) {
     /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/
 
   if (!emailRegExp.test(currentEmail)) {
-    setEmailMessage("이메일의 형식이 올바르지 않습니다!")
+    setEmailMessage("이메일의 형식이 올바르지 않습니다.")
   } else (
     setEmailMessage("")
   )
 }
 
-export const onChangePhoneNumber = function(e, setphoneNumber, setPhoneNumberMessage) {
-  const currentPhoneNumber = e.target.value
-  setphoneNumber(currentPhoneNumber)
-  const phoneRegExp = /^[0-9\b -]{0,13}$/
+export const onChangePhoneNumber = function(e, setPhoneNumber, setPhoneNumberMessage) {
+  const currentPhoneNumber = e.target.value;
+  
+  const regPhone = (phoneNumber) => {
+    return phoneNumber.replace(/[^0-9]/g, "").replace(/(\d{3})(\d{4})(\d)/, "$1-$2-$3");
+  };
+  
+  setPhoneNumber(regPhone(currentPhoneNumber));
+  const phoneRegExp = /^[0-9\b -]{0,13}$/;
 
-  if (!phoneRegExp.test(currentPhoneNumber)) {
-    setPhoneNumberMessage("숫자만 입력해 주세요.")
-  } else if (currentPhoneNumber.length > 11){
-    setPhoneNumberMessage("핸드폰 번호를 입력해 주세요.")
+  if (currentPhoneNumber.replace(/[^0-9]/g, "").length > 11) {
+    setPhoneNumberMessage("핸드폰 번호를 입력해 주세요.");
+  } 
+  else if (!phoneRegExp.test(currentPhoneNumber)) {
+    setPhoneNumberMessage("숫자만 입력해 주세요.");
   } else {
-    setPhoneNumberMessage("")
+    setPhoneNumberMessage("");
   }
-}
+};
 
 export const onChangeId = function(e, setId, setIdMessage) {
   const currentId = e.target.value
@@ -33,89 +39,58 @@ export const onChangeId = function(e, setId, setIdMessage) {
   const idRegExp = /^[a-zA-z0-9]{4,12}$/
 
   if (!idRegExp.test(currentId)) {
-    setIdMessage("4-12사이 대소문자 또는 숫자만 입력해 주세요!")
+    setIdMessage("4-12이내 영문 대소문자 또는 숫자만 입력해 주세요!")
   } else {
-    setIdMessage("사용가능한 아이디 입니다.")
+    setIdMessage("")
   }
 }
 
-export const onChangePassword = function(e, setPassword, setPasswordMessage) {
+export const onChangePassword = function(e, setPassword, setPasswordMessage, setIsPassword) {
   const currentPassword = e.target.value;
   setPassword(currentPassword);
   const passwordRegExp =
     /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/
   if (!passwordRegExp.test(currentPassword)) {
     setPasswordMessage(
-      "숫자+영문자+특수문자 조합으로 8자리 이상 입력해주세요."
+      "숫자, 영문, 특수문자 조합으로 8자리 이상 입력해주세요."
     );
+    setIsPassword(false)
   } else {
     setPasswordMessage("")
+    setIsPassword(true)
   }
 }
 
-export const onChangePasswordConfirm = function(e, password, setPasswordConfirm, setPasswordConfirmMessage) {
+export const onChangePasswordConfirm = function(e, password, setPasswordConfirm, setPasswordConfirmMessage, setIsPasswordConfirm) {
   const currentPasswordConfirm = e.target.value;
-  setPasswordConfirm(currentPasswordConfirm);
+  setPasswordConfirm(currentPasswordConfirm)
   if (password !== currentPasswordConfirm) {
     setPasswordConfirmMessage("같은 비밀번호를 입력해주세요.");
-    setPasswordConfirm(currentPasswordConfirm);
+    setIsPasswordConfirm(false)
   } else {
     setPasswordConfirmMessage("");
     setPasswordConfirm(currentPasswordConfirm);
+    setIsPasswordConfirm(true)
   }
 }
 
-export const onChangeExp = function(e, setExp, setExpMessage) {
+export const onChangeExp = function(e, setExp, setExpMessage, setIsExp) {
   const currentExp = e.target.value
   setExp(currentExp)
   const ExpReg = /^[0-9]{0,13}$/
   if (!ExpReg.test(currentExp)) {
-    setExpMessage(
-      "숫자만 입력해주세요."
-    );
+    setExpMessage("숫자만 입력해주세요.")
+    setIsExp(false)
   } else {
     setExpMessage("")
+    setIsExp(true)
   }
 }
 
 
-
-export const checkEmail = async (e, email) => {
-  e.preventDefault()
-  checkEmailApi(email)
-}
-
-export const checkId = async (e, id) => {
-  e.preventDefault()
-  checkIdApi(id)
-}
-
-
-
-export const clickSignup = async (e, data) => {
-  e.preventDefault()
-  signUp(data)
-  .then((response) => {
-    console.log(response)
-  }).catch((error) => {
-      console.log(error)
-  });
-}
-
-export const clickConsultantSignup = 
-async (e, data) => {
-  e.preventDefault()
-  consultSignUp(data)
-  .then((response) => {
-    console.log(response)
-  }).catch((error) => {
-      console.log(error)
-  });
-}
-
 export const clickLogin = async (e, data) => {
   e.preventDefault()
-  login(data)
+  await login(data)
   .then((response) => {
       localStorage.clear()
       localStorage.setItem('tokenType', response.tokenType)
