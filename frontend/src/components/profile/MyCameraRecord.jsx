b94e9a1e70411fcf8a0ae59b767f02d27e09dc3b
서지현 <gyg03134@naver.com>
2023-07-26 14:01:34 +0900

    Fix: add 누락된 부분 commit

--- frontend/src/components/profile/MyCameraRecord.jsx
+++ frontend/src/components/profile/MyCameraRecord.jsx
@@ -5,6 +5,39 @@ function MyCameraRecord() {
   let mediaRecorder;
   let recordedBlobs = [];
 
+  //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//   function startRecording() {
+//     recordedBlobs = [];
+//     const mimeType = codecPreferences.options[codecPreferences.selectedIndex].value;
+//     const options = {mimeType};
+  
+//     try {
+//       mediaRecorder = new MediaRecorder(window.stream, options);
+//     } catch (e) {
+//       console.error('Exception while creating MediaRecorder:', e);
+//       errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
+//       return;
+//     }
+  
+//     console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+//     recordButton.textContent = 'Stop Recording';
+//     playButton.disabled = true;
+//     downloadButton.disabled = true;
+//     codecPreferences.disabled = true;
+//     mediaRecorder.onstop = (event) => {
+//       console.log('Recorder stopped: ', event);
+//       console.log('Recorded Blobs: ', recordedBlobs);
+//     };
+//     mediaRecorder.ondataavailable = handleDataAvailable;
+//     mediaRecorder.start();
+//     console.log('MediaRecorder started', mediaRecorder);
+//   }
+  
+//   function stopRecording() {
+//     mediaRecorder.stop();
+//   }
+//   ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
   // 녹화 버튼 클릭 시 호출되는 함수
   const handleRecordButtonClick = () => {
     if (!recording) {
