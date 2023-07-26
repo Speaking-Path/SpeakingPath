3df2a0695198ea7090c36474d473238bbe0af12a
이은경 <just6515@gmail.com>
2023-07-26 17:33:39 +0900

    make_main + nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
 import './App.css';
-import NavBar from './components/NavBar/Navbar'
+// import NavBar from './components/NavBar/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import UserSignup from './pages/UserSignup'
 import ConsultantSignup from './pages/ConsultantSignup'
 import UserInformation from './components/profile/UserInformation'
 import Login from './pages/Login'
 import UntactConsult from './components/Video/Untact';
-
+import { NavBar } from './components/NavBar/Navbar';
+import MainPage from './pages/MainPage';
 
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar/> 
       {/* 루트 추가 여기에 */}
       <Routes>
+      <Route exact path="/" element={<MainPage/>} />
         <Route path="/account/signup" element={<UserSignup/>}/>
         <Route path="/account/consultantsignup" element={<ConsultantSignup/>}/>
         <Route path="/account/mypage" element={<UserInformation />}/>
