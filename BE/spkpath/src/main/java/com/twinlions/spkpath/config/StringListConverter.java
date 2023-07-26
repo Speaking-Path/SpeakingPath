6d1f8e0b18353baf2327646b949536d95dd62325
박은규 <parkeg1223@gmail.com>
2023-07-26 10:59:31 +0900

    Fix: Consultant list 속성 관련 stringconverter 수정

--- BE/spkpath/src/main/java/com/twinlions/spkpath/config/StringListConverter.java
+++ BE/spkpath/src/main/java/com/twinlions/spkpath/config/StringListConverter.java
@@ -1,5 +1,10 @@
 package com.twinlions.spkpath.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.persistence.AttributeConverter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -7,14 +12,27 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StringListConverter implements AttributeConverter<List<String>, String> {
-    private final String DELIMITER = ",";
+    private static final ObjectMapper objectMapper = new ObjectMapper()
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL);
     @Override
     public String convertToDatabaseColumn(List<String> attribute) {
-        return String.join(DELIMITER, attribute);
+        if (attribute == null) {
+            return null;
+        }
+
+        try {
+            return objectMapper.writeValueAsString(attribute);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException();
+        }
     }
 
     @Override
     public List<String> convertToEntityAttribute(String dbData) {
-        return new ArrayList<>(Arrays.asList(dbData.split(",")));
+        try {
+            return objectMapper.readValue(dbData, new TypeReference<List<String>>() {});
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
     }
 }
--- BE/spkpath/src/main/java/com/twinlions/spkpath/consultant/service/ConsultantServiceImpl.java
+++ BE/spkpath/src/main/java/com/twinlions/spkpath/consultant/service/ConsultantServiceImpl.java
@@ -6,6 +6,7 @@ import com.twinlions.spkpath.consultant.repository.ConsultantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
