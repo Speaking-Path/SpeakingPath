de1926f2f9faf7ccf211c376b18c0ab4f0ef2ff9
gunhoo <gunhoo2016@gmail.com>
2023-07-27 17:43:55 +0900

    Feat: Password Encoding
    - 비밀번호 암호화하여 DB 저장
    - TODO: 로그인(복호화)

--- BE/spkpath/src/main/java/com/twinlions/spkpath/config/SecurityConfig.java
+++ BE/spkpath/src/main/java/com/twinlions/spkpath/config/SecurityConfig.java
@@ -7,7 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -38,7 +41,9 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder getPasswordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        return new BCryptPasswordEncoder();
     }
+
 }
