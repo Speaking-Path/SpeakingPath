0d856d137ff9b87cb40031d738fc9e516ce087de
오연주 <siunaus224@gmail.com>
2023-07-27 17:51:45 +0900

    Fix: 네비게이션 화면표시 수정 로그인, 로그아웃 따른 화면표시 수정

--- frontend/src/components/NavBar/Navbar.jsx
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -45,43 +45,62 @@ function NavBar() {
             {
               loginToken ? (
                 <div>
-                <NavLink className={styles.lasttab}
-                  style={({ isActive }) => { return { fontWeight: isActive ? "bold" : "", color: isActive ? 'blue' : '', } }}
-                  to="account/mypage">
-                  프로필</NavLink>
+                  <NavLink className={styles.lasttab}
+                    style={({ isActive }) => { return { fontWeight: isActive ? "bold" : "", color: isActive ? 'blue' : '', } }}
+                    to="account/mypage">
+                    프로필</NavLink>
 
-                <NavLink className={styles.lasttab} onClick={()=>{
-                  dispatch(changeLoginInfo(""))
-                  console.log(loginToken)}}>
-                  로그아웃</NavLink>
-              </div>
-  
-               ) :
-              ( 
-                <div>
-                <NavLink className={`${styles.lasttab} ms-auto`}
-                  style={({ isActive }) => { return { fontWeight: isActive ? "bold" : "", color: isActive ? 'blue' : '', } }}
-                  to="account/signup">
-                  회원가입</NavLink>
+                  <NavLink className={styles.lasttab} onClick={() => {
+                    dispatch(changeLoginInfo(""))
+                    localStorage.clear()
+                  }}>
+                    로그아웃</NavLink>
+                </div>
 
-                <NavLink className={styles.lasttab}
-                  style={({ isActive }) => { return { fontWeight: isActive ? "bold" : "", color: isActive ? 'blue' : '', } }}
-                  to="account/login">
-                  로그인</NavLink>
-              </div>
-               )
-            } 
+              ) :
+                (
+                  <div>
+                    <NavLink className={`${styles.lasttab} ms-auto`}
+                      style={({ isActive }) => { return { fontWeight: isActive ? "bold" : "", color: isActive ? 'blue' : '', } }}
+                      to="account/signup">
+                      회원가입</NavLink>
+
+                    <NavLink className={styles.lasttab}
+                      style={({ isActive }) => { return { fontWeight: isActive ? "bold" : "", color: isActive ? 'blue' : '', } }}
+                      to="account/login">
+                      로그인</NavLink>
+                  </div>
+                )
+            }
 
 
 
           </Nav>
         </Container>
       </Navbar>
-      <div >
-
+      <div>
+        <ul>
+          <li>발음 연습하기</li>
+          <ul>
+            <li>음절 말하기</li>
+            <li>단어 말하기</li>
+            <li>문장 말하기</li>
+          </ul>
+        </ul>
+        <ul>
+          <li>인지 연습하기</li>
+          <ul>
+            <li>사물 고르기</li>
+            <li>사물 이름 맞히기</li>
+          </ul>
+        </ul>
+        <div>
+          <p>필요한</p>
+          <p>카테고리를</p>
+          <p>선택해</p>
+          <p>학습하세요</p>
+        </div>
       </div>
-      <p></p>
-      <p></p>
     </div>
   );
 }
