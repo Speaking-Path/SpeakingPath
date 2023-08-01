2a51f82c9a30dc23d384368202156134eaa0ba1e
박은규 <parkeg1223@gmail.com>
2023-08-01 17:55:45 +0900

    Feat: 상담사 예약 가능 시간 추가 기능 구현

--- BE/spkpath/src/main/java/com/twinlions/spkpath/consulting/service/ConsultingService.java
+++ BE/spkpath/src/main/java/com/twinlions/spkpath/consulting/service/ConsultingService.java
@@ -1,4 +1,7 @@
 package com.twinlions.spkpath.consulting.service;
 
+import com.twinlions.spkpath.consulting.AvailableInfoDto;
+
 public interface ConsultingService {
+    String addSchedule(AvailableInfoDto availableInfoDtoList);
 }
