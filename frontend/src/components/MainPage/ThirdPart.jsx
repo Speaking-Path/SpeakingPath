ef0ab3b067e5937dc75279537da0e00db590377e
Yeoni <siunaus224@gmail.com>
2023-08-06 19:03:34 +0900

    Fix: 메인페이지 메인페이지 클릭 시 페이지 이동 추가

--- frontend/src/components/MainPage/ThirdPart.jsx
+++ frontend/src/components/MainPage/ThirdPart.jsx
@@ -1,6 +1,9 @@
 import styles from './ThirdPart.module.css'
+import { useNavigate } from "react-router-dom"
 
 const ThirdPart = () => {
+  const navigate = useNavigate()
+
   return (
     <div>
       <p className={styles.ThirdPartTitle}>언어재활 바로가기</p>
@@ -8,7 +11,7 @@ const ThirdPart = () => {
         <div className={styles.ThirdPartText}>
           <p className={styles.Text1}>혼자서도 쉬운 발음 연습</p>
           <p className={styles.Text2}>발음 연습을 통해 정확한 발음을 연습하세요</p>
-          <p className={styles.Text3}>연습하러가기 &gt;</p>
+          <p className={styles.Text3} onClick={(e)=>{navigate("/practice")}}>연습하러가기 &gt;</p>
         </div>
         <div>
           <img className={styles.ThirdPartImg} src={process.env.PUBLIC_URL + "/assets/main/secondPart1.png"} alt="" />
@@ -18,7 +21,7 @@ const ThirdPart = () => {
         <div className={styles.ThirdPartText}>
           <p className={styles.Text1}>인지능력 연습</p>
           <p className={styles.Text2}>집에서도 꾸준한 연습을 통해 인지 능력을 키워보세요.</p>
-          <p className={styles.Text3}>연습하러가기 &gt;</p>
+          <p className={styles.Text3} onClick={(e)=>{navigate("/practice")}}>연습하러가기 &gt;</p>
         </div>
         <div>
           <img className={styles.ThirdPartImg} src={process.env.PUBLIC_URL + "/assets/main/secondPart2.png"} alt="" />
