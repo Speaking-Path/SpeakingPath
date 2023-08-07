524a019cd3995c3e16d6b8cc42dc295befcf9f96
오연주 <siunaus224@gmail.com>
2023-08-07 22:16:36 +0900

    Design: 훈련 css 추가 훈련 부분 css 추가

--- frontend/src/components/Practice/Recognition/PickNameMain.jsx
+++ frontend/src/components/Practice/Recognition/PickNameMain.jsx
@@ -1,10 +1,12 @@
 import Confetti from "../Confetti";
 import RetryRecog from "../RetryRecog";
 import { useState } from "react";
-import styles from './PickPicMain.module.css';
+// import styles from './PickPicMain.module.css';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import CelebrateRecog from "./CelebrateRecog";
+import styles from './PickNameMain.module.css'
+import { blue } from "@mui/material/colors";
 
 function PickPicMain({ qlist, num, goForward, goBack }) {
   const [isCorVisible, setIsCorVisible] = useState(false);
@@ -32,25 +34,31 @@ function PickPicMain({ qlist, num, goForward, goBack }) {
 
   return (
     <div className={styles.picMainBox}>
-      {answer.objName && <p>Q. 사진과 어울리는 단어를 골라주세요.</p>}
+      {answer.objName && <p className={styles.question}>Q. 사진과 어울리는 단어를 골라주세요.</p>}
       <div>
         {
-          num !== 0 && <ArrowBackIosNewIcon onClick={()=>{goBack()}}/>
+          num !== 0 && <ArrowBackIosNewIcon sx={{ color: blue[700], fontSize: 40 }}
+          onClick={()=>{goBack()}}/>
         }
         {answer.objId && <img src={process.env.PUBLIC_URL + "/assets/PickPic/" + answer.objId + ".jpg"}
           className={styles.imgBox} alt="" />}
         {
-          num !== 9 && <ArrowForwardIosIcon onClick={()=>{goForward()}}/>
+          num !== 9 && <ArrowForwardIosIcon sx={{ color: blue[700], fontSize: 40 }}
+          onClick={()=>{goForward()}}/>
         }
       </div>
-      <div>
+      <div className={`${styles.mapBox} container`}>
+        <div className="row">
         {
           questions.map((question, index) => (
-            <div key={index}>
-              <p onClick={() => { checkAns(question.objId) }}>{question.objName}</p>
+            <div onClick={() => { checkAns(question.objId) }} className={`${styles.map} col-6`} key={index}>
+              <p className={styles.index}>{index+1}</p>
+              <p className={styles.mapP} >
+                {question.objName}</p>
             </div>
           ))
         }
+        </div>
       </div>
       <div>
         {isCorVisible && <Confetti />}
