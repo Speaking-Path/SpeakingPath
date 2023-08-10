ba3575d32c0a3179f26b30add489948b98005e03
gunhoo <gunhoo2016@gmail.com>
2023-08-10 17:33:19 +0900

    Feat: 상담사 정보 반환 API 추가

--- BE/spkpath/src/main/java/com/twinlions/spkpath/consultant/service/ConsultantServiceImpl.java
+++ BE/spkpath/src/main/java/com/twinlions/spkpath/consultant/service/ConsultantServiceImpl.java
@@ -100,4 +100,15 @@ public class ConsultantServiceImpl implements ConsultantService {
         consultantDto.setCsltBoundaryFromList(consultant.getCsltBoundaries());
         return consultantDto;
     }
+
+    @Override
+    public ConsultantDto getCsltByUserId(String userId) {
+        try{
+            ConsultantDto dto = convertToDto(consultantRepository.findByUserId(userId));
+            dto.setUserPwd("encrypted");
+            return dto;
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
