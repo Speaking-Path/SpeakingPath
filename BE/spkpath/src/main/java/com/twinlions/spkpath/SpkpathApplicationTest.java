798d339cfbe71ad76e468cbebe95bf6c1170d2af
gunhoo <gunhoo2016@gmail.com>
2023-08-11 12:11:43 +0900

    Fix: 시작파일 변경

--- BE/spkpath/src/main/java/com/twinlions/spkpath/SpkpathApplicationTest.java
+++ BE/spkpath/src/main/java/com/twinlions/spkpath/SpkpathApplicationTest.java
@@ -2,13 +2,12 @@ package com.twinlions.spkpath;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(scanBasePackages = "com.twinlions.spkpath")
-public class SpkpathApplication {
+public class SpkpathApplicationTest {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpkpathApplication.class, args);
+		SpringApplication.run(SpkpathApplicationTest.class, args);
 	}
 
 }
