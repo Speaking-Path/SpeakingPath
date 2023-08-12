a60581d85495db22d7fffa9181fe2887881c7ee8
gunhoo <gunhoo2016@gmail.com>
2023-08-13 00:42:36 +0900

    Fix: redirect시 header 검증 추가

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect} from 'react'
+import axios from "axios"
 // import Header from '../components/Header';
 import Banner from '../components/MainPage/Banner';
 import Banner2 from '../components/MainPage/Banner2';
@@ -11,6 +12,25 @@ import Banner5 from '../components/MainPage/Banner5';
 
 
 const Main = () => {
+  useEffect(() => {
+    // 서버에서 정보를 가져오는 GET 요청을 보냅니다.
+    axios.get('https://i9c109.p.ssafy.io')
+      .then(response => {
+        const accessToken = response.headers['accessToken'];
+        const refreshToken = response.headers['refreshToken'];
+        const grantType = response.headers['grantType'];
+
+        if (accessToken && refreshToken && grantType) {
+          // 응답에서 필요한 정보가 모두 제공된 경우 localStorage에 저장합니다.
+          localStorage.setItem('accessToken', accessToken);
+          localStorage.setItem('refreshToken', refreshToken);
+          localStorage.setItem('grantType', grantType);
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
   return (
     <div>
       {/* <Header /> */}
