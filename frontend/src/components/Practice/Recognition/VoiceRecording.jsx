05fd2278eae835ce59aaeb6bfe6d06bbb52c256c
gunhoo <gunhoo2016@gmail.com>
2023-08-13 22:23:37 +0900

    Fix: stt axios 주소 변경

--- frontend/src/components/Practice/Recognition/VoiceRecording.jsx
+++ frontend/src/components/Practice/Recognition/VoiceRecording.jsx
@@ -179,8 +179,8 @@ const VoiceRecording = () => {
         const base64Audio = reader.result.split(',')[1]; // Extract base64 data
 
         // Send Base64 audio to the server
-        const sttServer = axios.create({ baseURL: "https://i9c109.p.ssafy.io:5001" });
-        const response = await sttServer.post('/stt/result', {
+        // const sttServer = axios.create({ baseURL: "https://i9c109.p.ssafy.io" });
+        const response = await axios.post('/stt/result', {
           file: base64Audio,
           // format: 'pcm'
         });
