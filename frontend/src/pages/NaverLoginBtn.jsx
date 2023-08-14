0f8314e7bf384636f22d6a52aa6d37b2729127e7
오연주 <siunaus224@gmail.com>
2023-08-14 13:35:25 +0900

    Design: 로그인 화면 네이버 로그인 버튼 변경 네이버 로그인 버튼 위치, 디자인 변경

--- frontend/src/pages/NaverLoginBtn.jsx
+++ frontend/src/pages/NaverLoginBtn.jsx
@@ -1,31 +1,47 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
+import styles from './NaverLoginBtn.module.css'
 
-function NaverLoginBtn () {
-    useEffect(() => {
-        let naverLogin = new window.naver.LoginWithNaverId({
-          clientId: `${process.env.REACT_APP_CLIENT_ID}`,
-          callbackUrl: `http://i9c109.p.ssafy.io/#/account/naverlogin`,
-          loginButton: { color: "green", type: 3, height: "50" },
-        });
-        naverLogin.init();
-        naverLogin.logout();
-        try {
-          naverLogin.getLoginStatus((status) => {
-            if (status) {
-              // console.log(naverLogin.user);
-            }else{
-              // console.log(naverLogin)
-            }
-          });
-        } catch (err) {
-          console.log(err);
+function NaverLoginBtn() {
+  const naverRef = useRef()
+  useEffect(() => {
+    let naverLogin = new window.naver.LoginWithNaverId({
+      clientId: `${process.env.REACT_APP_CLIENT_ID}`,
+      callbackUrl: `http://i9c109.p.ssafy.io/#/account/naverlogin`,
+      loginButton: { color: "green", type: 3, height: "45" },
+    });
+    naverLogin.init();
+    naverLogin.logout();
+    try {
+      naverLogin.getLoginStatus((status) => {
+        if (status) {
+          // console.log(naverLogin.user);
+        } else {
+          // console.log(naverLogin)
         }
-      }, []);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
-    return (
-        <div id="naverIdLogin"></div>
-    )
-      
+  useEffect(() => {
+    naverRef.current.children[0].style.display = 'none';
+  }, []);
+  
+  const handleClick = () => {
+    naverRef.current.children[0].click();
+  }
+
+  return (
+    <>
+      <div ref={naverRef} id="naverIdLogin"></div>
+      <button onClick={handleClick} className={styles.naver} >
+        <img src={process.env.PUBLIC_URL + "/assets/naver.png"} alt="naver" />
+        네이버 로그인
+      </button>
+    </>
+  )
+
 }
 
 export default NaverLoginBtn
