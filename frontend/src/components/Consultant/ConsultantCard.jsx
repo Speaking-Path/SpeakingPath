b877a77d42063520a5fbe2657ac027c2c2650858
gunhoo <gunhoo2016@gmail.com>
2023-08-16 10:34:13 +0900

    Fix: 상담사 리스트에서 이미지 오류 해결

--- frontend/src/components/Consultant/ConsultantCard.jsx
+++ frontend/src/components/Consultant/ConsultantCard.jsx
@@ -38,7 +38,7 @@ function ConsultantCard({ consultant }) {
     <div className={styles.csltCard}>
       <div className={styles.cardBox}>
         <div className={styles.profileBox}>
-          <img className={styles.profileImage} src={process.env.PUBLIC_URL + imgsrc} alt="" />
+          <img className={styles.profileImage}  src={process.env.PUBLIC_URL + "/assets/profile/" + (consultant.userPic !== null ? consultant.userPic : 'user.png')} alt="" />
         </div>
         <div className={styles.cardInfo}>
           <div className={styles.nameAndExp}>
