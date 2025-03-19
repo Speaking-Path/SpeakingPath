63184293b121e360bc2e02842f7a43f45a6eed7b
Gunhoo Park <gunhoo2016@gmail.com>
2025-03-18 22:28:47 -0700

    fix: remove comment

--- BE/spkpath/src/main/java/com/twinlions/spkpath/user/repository/UserRepository.java
+++ BE/spkpath/src/main/java/com/twinlions/spkpath/user/repository/UserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.Repository;
 import java.util.Optional;
 
 public interface UserRepository extends Repository<User, String> {
-//    Optional<Object> findById(String userId);
     Optional<User> findByUserId(String userId);
     Optional<Object> findByUserEmail(String userEmail);
     Optional<User> findByUserNameAndUserEmail(String userName, String userEmail);
